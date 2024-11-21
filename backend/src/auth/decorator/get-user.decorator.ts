07801e6fb8340a1b26089aace41fdeaa661835df
Ardelingga Pramesta Kusuma <ardelingga@sangkuriang.co.id>
2024-11-21 20:49:42 +0700

    Init backend project

---
+++ backend/src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request: Express.Request = ctx.switchToHttp().getRequest();
+    
+    console.log("PRINT DATA: ");
+    console.log(data);
+    if (data) {
+      return request.user[data];
+    }
+    return request.user;
+  },
+);
